c0b026eb0112f7f2ccae16d0e7f9753c4d9d1d52
seung-je <csz0209@gmail.com>
2025-06-18 20:59:41 +0900

    Initial commit

---
+++ src/main/java/com/mysite/jpa_shoppingmall/JpaShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.jpa_shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShoppingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShoppingmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/jpa_shoppingmall/JpaShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.jpa_shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
