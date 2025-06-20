e46cdf335dc8098bde7b7be8d61abccbbb847457
seung-je <csz0209@gmail.com>
2025-06-20 23:33:28 +0900

    🐛bug: Resolve entity table creation failure in prod profile
    The `spring.jpa.hibernate.ddl-auto` setting in the common profile (`none`) was overriding the setting in the prod profile (`create`), which prevented entity tables from being generated.
    
    Removed the environment-specific `ddl-auto` property from `application-common.yml` to allow the profile-specific setting to take effect.

---
+++ src/main/java/com/mysite/jpa_shoppingmall/item/entity/Item.java
@@ -0,0 +1,39 @@
+package com.mysite.jpa_shoppingmall.item.entity;
+
+import com.mysite.jpa_shoppingmall.item.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // DB가 변경되더라도 코드 수정 필요 x(but, dev와 prod DB가 다를 경우 예기치 못한 에러가 발생할 수 있음)
+    private Long id; // 상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; // 상품명
+
+    @Column(nullable = false)
+    private int price; // 가격
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태
+    private LocalDateTime regTime; // 등록 시간
+    private LocalDateTime updateTime; // 수정 시간
+
+    /*
+    실제로는 1개의 상품에 여러가지 옵션 및 옵션 상품의 가격, 재고, 배송 방법에 대한 정보까지 관리해야함
+     */
+}
